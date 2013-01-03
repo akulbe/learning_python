c769d1db7860f88eeac30ee76be97136b85ac769
Aaron Kulbe <aaron@longviewbits.com>
2013-01-03 07:48:03 -0800

    Cleaned up syntax

--- convert.py
+++ convert.py
@@ -5,9 +5,10 @@ __author__ = 'Aaron Kulbe'
 #
 # Note: this isn't a copy & paste. I'm typing it in exactly as I found it in the John Zelle book.
 
+
 def main():
     celsius = eval(input("What is the Celsius temperature? "))
-    fahrenheit = 9/5 * celsius + 32
+    fahrenheit = 9 / 5 * celsius + 32
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
 
 main()
